9c1610b6b71d1ae94de8bbe144f8e25d53903a8d
audgeviolin07 <123830780+audgeviolin07@users.noreply.github.com>
2024-04-11 15:12:48 -0400

    Test commit

--- petqs-frontend/src/app/api /send/route.js
+++ petqs-frontend/src/app/api /send/route.js
@@ -15,7 +15,7 @@ export async function POST(req, res) {
       react: (
         <>
           <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
+          <p>Thanks for contacting us!</p>
           <p>New message submitted:</p>
           <p>{message}</p>
         </>
