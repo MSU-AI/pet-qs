6674ca1d7692c1681021e5d27c4ff51b96fecb3d
Owen-Cochell <owencochell@gmail.com>
2024-04-13 22:34:59 -0400

    changed gunicorn port config

--- petqs-backend/gunicorn_production.conf.py
+++ petqs-backend/gunicorn_production.conf.py
@@ -21,7 +21,7 @@ workers = 2
 
 # The socket to bind:
 bind = "10.8.0.2:4455"  # If running on Owen Machine
-#bind = "127.0.0.1:445"  # If running locally/on production
+#bind = "127.0.0.1:4455"  # If running locally/on production
 #bind = "unix:/var/www/html/cardiB/cardiB.sock"
 
 # Write access and error info to /var/log
